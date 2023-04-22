679e28d185109054d51bcee7ec51c2ebbaeec49a
Laisens <kenny.sergey@gmail.com>
2023-04-22 11:59:26 +0200

    Update gameSnake.cpp

--- gameSnake.cpp
+++ gameSnake.cpp
@@ -3,8 +3,25 @@
 
 #include <iostream>
 using namespace std;
+char playingField[10][10];
 
 int main()
 {
-	cout << " Hello, World!";
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			playingField[i][j]=' ';
+		}
+		
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << playingField[i][j];
+		}
+		cout << endl;
+	}
+	
 }
